a7c4f2d1967be1b569ff4abbd691d233a37f8d9b
LuckyPuppy514 <122154133@qq.com>
2025-06-15 11:57:12 +0800

    v1.2.1 修复同步时间无法关闭的问题

--- external-player.user.js
+++ external-player.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN              外部播放器
 // @namespace               https://github.com/LuckyPuppy514/external-player
 // @copyright               2024, Grant LuckyPuppy514 (https://github.com/LuckyPuppy514)
-// @version                 1.2.0
+// @version                 1.2.1
 // @license                 MIT
 // @description             Play web video via external player
 // @description:zh-CN       使用外部播放器播放网页中的视频
@@ -35,7 +35,7 @@ const VIDEO_URL_REGEX_EXACT = /^https?:\/\/((?![^"^']*http)[^"^']+(\.|%2e)(mp4|m
 
 const defaultConfig = {
     global: {
-        version: '1.2.0',
+        version: '1.2.1',
         language: (navigator.language || navigator.userLanguage) === 'zh-CN' ? 'zh' : 'en',
         buttonXCoord: '0',
         buttonYCoord: '0',
@@ -250,6 +250,9 @@ class BaseParser {
     }
     async parseTime() {
         try {
+            if (!currentPlayer.presetEvent.syncTime) {
+                return;
+            }
             for (const video of document.getElementsByTagName('video')) {
                 currentMedia.time = video.currentTime;
                 return;
